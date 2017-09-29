fb63fbbaabb502505a344e0e7781673a9e283cdf
vihar <vihar.kurama@gmail.com>
2017-09-29 18:22:54 +0530

    static rearrangement

--- blogger/settings.py
+++ blogger/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -142,6 +143,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'assets', 'collected-static')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 LOGIN_REDIRECT_URL = '/'
 ACCOUNT_EMAIL_VERIFICATION = "none"
